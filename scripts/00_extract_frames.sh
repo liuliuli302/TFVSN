bb48816a29d47474ea7b7a96e36306459f1483e0
allets <liul29956@gmail.com>
2024-12-21 15:43:50 +0800

    123

--- scripts/00_extract_frames.sh
+++ scripts/00_extract_frames.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-dataset_dir="$HOME/workspace/TFVSN/data/SumMe"
+dataset_dir="$HOME/autodl-tmp/data/TVSum"
 
 # Set paths
 videos_dir="${dataset_dir}/videos"
 frames_dir="${dataset_dir}/frames"
 annotations_file="${dataset_dir}/annotations/test.txt"
 
-python src/util/extract_frames.py \
+python processing/extract_frames.py \
     --videos_dir "$videos_dir" \
     --frames_dir "$frames_dir" \
     --annotations_file "$annotations_file"
